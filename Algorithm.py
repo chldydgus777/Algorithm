eee2644a79e79d619a90cf8927c26fbd34b4190b
chldydgus777 <chldydgus777@naver.com>
2021-03-12 10:17:11 +0900

    0312_1

--- Algorithm.py
+++ Algorithm.py
@@ -151,19 +151,31 @@
 
 #소수 구하기
 #----------------------------------------1929----------
-def prime(num):
-    if num < 2:
-        return False 
+# def prime(num):
+#     if num < 2:
+#         return False 
 
-    i = 2 
-    while i * i <= num :
-        if num % i == 0:
-            return False
-        i += 1
-    return True
+#     i = 2 
+#     while i * i <= num :
+#         if num % i == 0:
+#             return False
+#         i += 1
+#     return True
 
-m, n = map(int, input().split())
+# m, n = map(int, input().split())
 
-for i in range(m, n+1 ):
-    if prime(i):
-        print(i)
+# for i in range(m, n+1 ):
+#     if prime(i):
+#         print(i)
+
+
+
+n = int(input())
+n = int(input())
+n = int(input())
+n = int(input())
+n = int(input())
+n = int(input())
+
+for i in (n):
+    print(i)
